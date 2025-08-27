3ceda4815d6cca5bf937638d1f0358da74482d45
mmichtrx <mmanning6206@gmail.com>
2025-08-27 09:50:38 -0500

    Update CW-1(3).py

--- CW-1(3).py
+++ CW-1(3).py
@@ -4,4 +4,7 @@ wages = int(input("Enter amount of wages: "))
 hours = int(input("Enter amount of hours: "))
 days = int(input("Enter amount of days: "))
 
-total_salary
+
+total_salary = wages * hours * days
+
+print("Your total salary is,", total_salary /5)
